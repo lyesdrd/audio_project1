5f3cfeee343e8562fea15a0685fcf032b1d79fee
lyesdrd <lyes9360@gmail.com>
2021-11-05 13:10:39 +0100

    l'apli foctionne il manque l'affichage

--- src/ui/SignalView.java
+++ src/ui/SignalView.java
@@ -1,12 +1,38 @@
 package ui;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 
 import javax.sound.sampled.*;
+import java.util.ArrayList;
 
-public class SignalView extends LineChart<Number,Number> {
+public class SignalView extends LineChart<Number,Number>  {
+
+    private Axis<Number> Xaxis;
+    private Axis<Number> Yaxis;
 
     public SignalView(Axis<Number> axis, Axis<Number> axis1) {
         super(axis, axis1);
     }
+
+    public void updateData(Axis<Number> newax){
+        this.Xaxis= newax;
+    }
+
+    public LineChart<Number,Number> makeChart(){
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setLabel("No of employees");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Revenue per employee");
+
+        LineChart lineChart = new LineChart(xAxis, yAxis);
+
+        return lineChart;
+    }
+
+
+
+
+
 }
