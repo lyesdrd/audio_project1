b938a77a9a21db573242e33c9f9c516b6ad7b39a
lyesdrd <lyes9360@gmail.com>
2021-11-05 02:37:22 +0100

    je travail sur l'affichage

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
 import java.util.Arrays;
 
 
@@ -19,18 +20,16 @@ public class Main extends Application {
     private AudioProcessor as;
     private String inline;
     private String outline;
+    private int frameSize ;
+    private int sampleRate;
 
-    public void setInline(String inline) {
-        this.inline = inline;
-        System.out.println(this.inline);
+    public void setAll(String inline,String outline,String framesize,String samplerate){
+        this.inline= inline;
+        this.outline= outline;
+        this.sampleRate= Integer.parseInt(samplerate);
+        System.out.println(sampleRate);
+        this.frameSize= Integer.parseInt(framesize);
     }
-
-    public void setOutline(String outline) {
-        this.outline = outline;
-        System.out.println(this.outline);
-    }
-
-
 
     public void setAs (AudioProcessor as) {
         this.as = as;
@@ -52,28 +51,47 @@ public class Main extends Application {
              }
 
          private Node createToolbar(){
+
              Button buttonstart = new Button("start");
              Button buttonstop = new Button("stop");
              ToolBar tb = new ToolBar(buttonstart,buttonstop, new Separator(), new Label("input mixer"));
+
+
              ComboBox<String> cbinput = new ComboBox<>();
                 Arrays.stream(AudioSystem.getMixerInfo()).forEach(e -> cbinput.getItems().add(e.getName())); // on remplit la combo box avec les differents mixers disponibles et l'utilisateur devra choisir un mixeur d'input
                         tb.getItems().add(cbinput);
-             Button buttonvaliderinput = new Button("valider");
-                buttonvaliderinput.setOnAction(event -> setInline(cbinput.getValue()));
-                        tb.getItems().add(buttonvaliderinput);
                         tb.getItems().add(new Separator());
+
+
 
                         tb.getItems().add(new Label("output mixer"));
              ComboBox<String> cboutput = new ComboBox<>();
                 Arrays.stream(AudioSystem.getMixerInfo()).forEach(e -> cboutput.getItems().add(e.getName())); //on remplit la combo box avec les differents mixers disponibles et l'utilisateur devra choisir un mixeur d'output
                         tb.getItems().add(cboutput); //on ajoute cette combo box à la fenêtre
-             Button buttonvalideroutput = new Button("valider");
-                buttonvalideroutput.setOnAction(event -> setOutline(cboutput.getValue()));
-                        tb.getItems().add(buttonvalideroutput);
                         tb.getItems().add(new Separator());
 
 
-             buttonstart.setOnAction(event -> System.out.println("start" ));
+             TextField framesize =new TextField("");
+                        tb.getItems().add(new Label("framesize"));
+                        tb.getItems().add(framesize);
+                        tb.getItems().add(new Separator());
+
+             TextField samplerate =new TextField("");
+                        tb.getItems().add(new Label("framerate"));
+                        tb.getItems().add(samplerate);
+                        tb.getItems().add(new Separator());
+
+             Button buttonvalider = new Button("valider");
+                buttonvalider.setOnAction(event -> setAll(cbinput.getValue(),cboutput.getValue(),framesize.getText(),samplerate.getText()));
+                        tb.getItems().add(buttonvalider);
+
+             buttonstart.setOnAction(event -> {
+                 try {
+                     setAs(AudioIO.startAudioProcessing(this.inline,this.outline,this.sampleRate,this.frameSize));
+                 } catch (LineUnavailableException e) {
+                     e.printStackTrace();
+                 }
+             });
              buttonstop.setOnAction(event -> AudioIO.stopAudioProcessing(as));
              return tb;
              }
